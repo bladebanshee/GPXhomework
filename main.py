242bb8e3430e0f064d54115ef14231ef3f32e978
claynevermoreee <vladimirgo3113@gmail.com>
2024-11-26 22:41:37 +0300

    Добавление функции для выбора файла через tkinter

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ import gpxpy
 import gpxpy.gpx
 from geopy.distance import geodesic
 import matplotlib.pyplot as plt
+# Функция для выбора файла через tkinter
+def select_file():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(filetypes=[("GPX files", "*.gpx")])
+    return file_path
