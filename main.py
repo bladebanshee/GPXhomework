43908474c474ed0f11dee0db1833e27b7fb4b1f4
claynevermoreee <vladimirgo3113@gmail.com>
2024-11-26 22:45:11 +0300

    Добавление в файл функций для расчета расстояний

--- main.py
+++ main.py
@@ -13,3 +13,23 @@ def select_file():
     root.withdraw()
     file_path = filedialog.askopenfilename(filetypes=[("GPX files", "*.gpx")])
     return file_path
+# Функции для расчетов расстояния
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371e3  # Радиус Земли в метрах
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    delta_phi = math.radians(lat2 - lat1)
+    delta_lambda = math.radians(lon2 - lon1)
+
+    a = math.sin(delta_phi / 2) ** 2 + math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    return R * c
+
+def great_circle_distance(lat1, lon1, lat2, lon2):
+    R = 6371e3  # Радиус Земли в метрах
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    delta_lambda = math.radians(lon2 - lon1)
+    return R * math.acos(math.sin(phi1) * math.sin(phi2) + math.cos(phi1) * math.cos(phi2) * math.cos(delta_lambda))
+
