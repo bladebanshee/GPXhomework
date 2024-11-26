456c0739b9dbfea2098c0a0b642ff89e29bd6874
claynevermoreee <vladimirgo3113@gmail.com>
2024-11-26 22:38:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# Импорты библиотек
+import os
+import math
+import tkinter as tk
+from tkinter import filedialog
+import gpxpy
+import gpxpy.gpx
+from geopy.distance import geodesic
+import matplotlib.pyplot as plt
