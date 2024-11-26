cdd44931b9dfb260120912c6a79409c40e305757
claynevermoreee <vladimirgo3113@gmail.com>
2024-11-26 22:46:22 +0300

    Добавление обработки gpx файла

--- main.py
+++ main.py
@@ -33,3 +33,10 @@ def great_circle_distance(lat1, lon1, lat2, lon2):
     delta_lambda = math.radians(lon2 - lon1)
     return R * math.acos(math.sin(phi1) * math.sin(phi2) + math.cos(phi1) * math.cos(phi2) * math.cos(delta_lambda))
 
+# Обработка GPX файла
+file_path = select_file()
+if not file_path or not os.path.exists(file_path):
+    print("Файл не выбран или не существует!")
+else:
+    with open(file_path, 'r', encoding='utf-8') as gpx_file:
+        gpx = gpxpy.parse(gpx_file)
